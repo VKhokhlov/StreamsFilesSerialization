dee9f95437fe3b594e23e73938476ceecbbe1051
Vladimir Khokhlov <vladimirkhokhlov@yandex.ru>
2022-09-10 10:08:36 +0700

    Added solution of Task 1

---
+++ src/Basket.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Basket {
+    private List<Item> items = new ArrayList<>();
+
+    private static class Item {
+        Product product;
+        int amount;
+
+        public Item(String saveString) {
+            String[] values = saveString.split(" ");
+            this.product = new Product(values[0], Integer.parseInt(values[1]));
+            this.amount = Integer.parseInt(values[2]);
+        }
+
+        public Item(Product product, int amount) {
+            this.product = product;
+            this.amount = amount;
+        }
+
+        public int sum() {
+            return product.getPrice() * amount;
+        }
+
+        public void print() {
+            System.out.println(product.getName() + " " + amount + " шт " + product.getPrice() + " руб/шт " + sum() + " руб в сумме");
+        }
+
+        public String save() {
+            return product.getName() + " " + product.getPrice() + " " + amount;
+        }
+    }
+
+    public void addToCart(Product product, int amount) {
+        Optional<Item> optionalItem = items.stream().filter(item -> item.product.equals(product)).findFirst();
+
+        if (optionalItem.isPresent()) {
+            optionalItem.get().amount += amount;
+        }
+        else {
+            items.add(new Item(product, amount));
+        }
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+
+        items.forEach(Item::print);
+
+        System.out.println("Итого: " + items.stream().mapToInt(Item::sum).sum() + " руб");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter writer = new PrintWriter(textFile)) {
+            items.forEach(item -> writer.println(item.save()));
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        Basket basket = new Basket();
+
+        if (!textFile.exists()) {
+            return basket;
+        }
+
+//        BufferedReader reader = new BufferedReader(new FileReader(textFile));
+//        String line = null;
+//
+//        while ((line = reader.readLine()) != null) {
+//            basket.items.add(new Basket.Item(line));
+//        }
+
+        Files.readAllLines(Path.of(textFile.getAbsolutePath())).forEach(line -> basket.items.add(new Basket.Item(line)));
+
+        return basket;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        List<Product> products = Arrays.asList(
+                new Product("Хлеб", 30),
+                new Product("Молоко", 85),
+                new Product("Гречка", 70)
+        );
+
+        File basketFile = new File("Basket.txt");
+        Basket basket;
+
+        try {
+            basket = Basket.loadFromTxtFile(basketFile);
+        }
+        catch (IOException e) {
+            System.out.println("Произошла ошибка при загрузке корзины из файла: " + e.getMessage());
+
+            basket = new Basket();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        printProducts(products);
+
+        while (true) {
+            System.out.println("Выберите товар и количество или введите `end`.");
+
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+
+            if (parts.length < 2) {
+                System.out.println("Не верный ввод!");
+                continue;
+            }
+
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+
+            if (productNumber > products.size() - 1) {
+                System.out.println("Не верный номер продукта!");
+                continue;
+            }
+
+            basket.addToCart(products.get(productNumber), productCount);
+
+            try {
+                basket.saveTxt(basketFile);
+            }
+            catch (IOException e) {
+                System.out.println("Произошла ошибка при сохранении корзины в файл: " + e.getMessage());
+            }
+        }
+
+        basket.printCart();
+    }
+
+    public static void printProducts(List<Product> products) {
+        System.out.println("Список возможных товаров для покупки:");
+
+        IntStream.range(0, products.size()).forEach(i -> System.out.println((i + 1) + ". " + products.get(i)));
+    }
+}
